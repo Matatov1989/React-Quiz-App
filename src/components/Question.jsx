271ed177693d29ecbd7d5b459216bbef1f1c99d1
Yury <matatov1989@gmail.com>
2023-11-02 18:37:47 +0200

    moving logic to components that actually need it (moving state down)

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,26 +1,54 @@
+import { useState } from "react";
+
 import Answers from "./Answers.jsx";
 import QuestionTimer from "./QuestionTimer.jsx";
+import QUESTIONS from "../questions.js";
 
 export default function Question({
-  questionText,
-  answers,
+  index,
   onSelectAnswer,
-  selectedAnswer,
-  answerState,
-  onSkipAnswer
+  onSkipAnswer,
 }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = '';
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
+
   return (
     <div id="question">
-      <QuestionTimer
-        timeout={10000}
-        onTimeout={onSkipAnswer}
-      />
-      <h2>{questionText}</h2>
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{QUESTIONS[index].text}</h2>
       <Answers
-        answers={answers}
-        selectedAnswers={selectedAnswer}
+        answers={QUESTIONS[index].answers}
+        selectedAnswers={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
