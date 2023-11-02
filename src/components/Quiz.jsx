48d21382996ff22c8c3fa3948f54f809c5a6d3cd
Yury <matatov1989@gmail.com>
2023-11-02 15:21:07 +0200

    output questions & registerig answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -15,17 +15,18 @@ export default function Quiz() {
 
   return (
     <div id="quiz">
-           <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndes].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndes].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
-    </div> 
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndes].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndes].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-
   );
 }
